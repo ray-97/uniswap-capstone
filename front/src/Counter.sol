522cdedf54c1fe04ec98f9f9a16bfa52e4d071d8
ray-97 <something.ray97@gmail.com>
2024-12-05 11:15:20 +0800

    fix: imports and remappings

--- front/src/Counter.sol
+++ front/src/Counter.sol
@@ -1,14 +1,132 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
-contract Counter {
-    uint256 public number;
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {SafeCast} from "v4-core/libraries/SafeCast.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+// Make sure to update the interface when Stylus Contract's Solidity ABI changes.
+interface IUniswapCurve {
+    function getAmountInForExactOutput(uint256 amountOut, address input, address output, bool zeroForOne)
+        external
+        returns (uint256);
+
+    function getAmountOutFromExactInput(uint256 amountIn, address input, address output, bool zeroForOne)
+        external
+        returns (uint256);
+}
+
+contract Counter is BaseHook {
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using SafeCast for uint256;
+
+    // NOTE: ---------------------------------------------------------
+    // state variables should typically be unique to a pool
+    // a single hook contract should be able to service multiple pools
+    // ---------------------------------------------------------------
+
+    mapping(PoolId => uint256 count) public beforeSwapCount;
+    mapping(PoolId => uint256 count) public afterSwapCount;
+
+    mapping(PoolId => uint256 count) public beforeAddLiquidityCount;
+    mapping(PoolId => uint256 count) public beforeRemoveLiquidityCount;
+
+    IUniswapCurve _curveContract;
+
+    constructor(IPoolManager _poolManager, address _curveContractAddress) BaseHook(_poolManager) {
+        _curveContract = IUniswapCurve(_curveContractAddress);
     }
 
-    function increment() public {
-        number++;
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: true,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: true,
+            afterRemoveLiquidity: false,
+            beforeSwap: true, // -- Custom Curve Handler --  //
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false, // -- Enable Custom Curves here if needed --  //
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    // -----------------------------------------------
+    // NOTE: see IHooks.sol for function documentation
+    // -----------------------------------------------
+
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
+        external
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        // You can add, modify, or delete the logic here if needed.
+
+        bool exactInput = params.amountSpecified < 0;
+        (Currency specified, Currency unspecified) =
+            (params.zeroForOne == exactInput) ? (key.currency0, key.currency1) : (key.currency1, key.currency0);
+
+        uint256 specifiedAmount = exactInput ? uint256(-params.amountSpecified) : uint256(params.amountSpecified);
+        uint256 unspecifiedAmount;
+
+        BeforeSwapDelta returnDelta;
+
+        if (exactInput) {
+            unspecifiedAmount = _curveContract.getAmountOutFromExactInput(
+                specifiedAmount, Currency.unwrap(specified), Currency.unwrap(unspecified), params.zeroForOne
+            );
+
+            returnDelta = toBeforeSwapDelta(specifiedAmount.toInt128(), -unspecifiedAmount.toInt128());
+        } else {
+            unspecifiedAmount = _curveContract.getAmountInForExactOutput(
+                specifiedAmount, Currency.unwrap(unspecified), Currency.unwrap(specified), params.zeroForOne
+            );
+
+            returnDelta = toBeforeSwapDelta(-specifiedAmount.toInt128(), unspecifiedAmount.toInt128());
+        }
+
+        beforeSwapCount[key.toId()]++;
+        return (BaseHook.beforeSwap.selector, returnDelta, 0);
+    }
+
+    function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
+        external
+        override
+        returns (bytes4, int128)
+    {
+        afterSwapCount[key.toId()]++;
+        return (BaseHook.afterSwap.selector, 0);
+    }
+
+    function beforeAddLiquidity(
+        address,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) external override returns (bytes4) {
+        beforeAddLiquidityCount[key.toId()]++;
+        return BaseHook.beforeAddLiquidity.selector;
+    }
+
+    function beforeRemoveLiquidity(
+        address,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) external override returns (bytes4) {
+        beforeRemoveLiquidityCount[key.toId()]++;
+        return BaseHook.beforeRemoveLiquidity.selector;
     }
 }
